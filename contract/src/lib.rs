16814f075f95835fa30eb8539f0f0719cd4bac0e
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2022-07-28 03:11:43 +0000

    update, fmt

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -7,9 +7,12 @@ use crate::songs_purchased::add_song_to_user_dashboard;
  *
  */
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen, env, AccountId,
-                serde::{Deserialize, Serialize}};
 use near_sdk::{collections::*, Promise};
+use near_sdk::{
+    env, log, near_bindgen,
+    serde::{Deserialize, Serialize},
+    AccountId,
+};
 
 // Define the contract structure
 #[near_bindgen]
@@ -28,10 +31,8 @@ pub struct SongList {
 }
 
 impl Default for SongList {
-    fn default() -> Self{
-        SongList{
-            songs: Vec::new(),
-        }
+    fn default() -> Self {
+        SongList { songs: Vec::new() }
     }
 }
 
@@ -39,7 +40,7 @@ impl Default for SongList {
 #[serde(crate = "near_sdk::serde")]
 pub struct SongInfo {
     song_name: String,
-    price: f32
+    price: f32,
 }
 
 // 1 Ⓝ in yoctoNEAR
@@ -47,10 +48,10 @@ pub struct SongInfo {
 
 // Define the default, which automatically initializes the contract
 
-impl Default for Contract{
-    fn default() -> Self{
+impl Default for Contract {
+    fn default() -> Self {
         log!("default!");
-        Self{
+        Self {
             songs_by_artist: UnorderedMap::new(b"s".to_vec()),
         }
     }
@@ -59,7 +60,7 @@ impl Default for Contract{
 // Implement the contract structure
 #[near_bindgen]
 impl Contract {
-    /* 
+    /*
     #[init]
     pub fn new() -> Self {
         log!("#[init]");
@@ -68,17 +69,31 @@ impl Contract {
         }
     }
     */
-    
+
     //Public method - accepts song name and price, and adds aong to artist's catalog
     pub fn add_song_info(&mut self, song_name: String, price: f32) {
-       let get_songinfo = self.songs_by_artist.get(&env::predecessor_account_id()).unwrap_or_default();
-       log!("songinfo: {:?}", &get_songinfo);
-       let mut get_song_list = get_songinfo.songs;
-       let mut song_list = get_song_list;
-       song_list.push(SongInfo{song_name: song_name, price});
-       self.songs_by_artist.insert(&env::predecessor_account_id(), 
-       &SongList{songs: song_list});
-       log!("songlist: {:?}", self.songs_by_artist.get(&env::predecessor_account_id()).unwrap().songs);
+        let get_songinfo = self
+            .songs_by_artist
+            .get(&env::predecessor_account_id())
+            .unwrap_or_default();
+        log!("songinfo: {:?}", &get_songinfo);
+        let get_song_list = get_songinfo.songs;
+        let mut song_list = get_song_list;
+        song_list.push(SongInfo {
+            song_name: song_name,
+            price,
+        });
+        self.songs_by_artist.insert(
+            &env::predecessor_account_id(),
+            &SongList { songs: song_list },
+        );
+        log!(
+            "songlist: {:?}",
+            self.songs_by_artist
+                .get(&env::predecessor_account_id())
+                .unwrap()
+                .songs
+        );
     }
     //Public method - returns entire song catalog
     //pub fn get_song_catalog(&self, account_id: AccountId) -> SongList {
@@ -88,13 +103,13 @@ impl Contract {
     }
 
     #[payable]
-    pub fn buy_song (artist: AccountId, song_name: String, price: f32) {
+    pub fn buy_song(artist: AccountId, song_name: String, price: f32) {
         Promise::new(artist).transfer(env::attached_deposit());
         log!("song purchased {} {}", song_name, env::attached_deposit());
-        add_song_to_user_dashboard(env::predecessor_account_id(), song_name);
+        add_song_to_user_dashboard(&env::predecessor_account_id(), song_name);
     }
 
-    /* 
+    /*
     pub fn remove_song_info(&mut self, account_id:AccountId, key:u8) {
         assert(caller==same_acct_that added_song)
         let get_songinfo = self.songs_by_artist.get(&account_id).unwrap_or_default();
@@ -114,7 +129,7 @@ impl Contract {
 #[cfg(test)]
 mod tests {
     use super::*;
-/*
+    /*
     #[test]
     #[ignore]
     fn get_default_greeting() {
--- contract/src/songs_purchased.rs
+++ contract/src/songs_purchased.rs
@@ -1,8 +1,15 @@
 use near_sdk::AccountId;
 
 //contains code for storing and retrieving song info a user has purchased
-use  near_sdk::log;
+use near_sdk::log;
 
-pub fn add_song_to_user_dashboard(user_id: AccountId, song_name: String) {
+//user songlist struct, vec<songs>
+//hashmap
+
+pub fn add_song_to_user_dashboard(user_id: &AccountId, song_name: String) {
     log!("{} {}", user_id, song_name);
+    //hashmap add songs by user
+    //hashmap key: user_id, value: Vec <songs>
 }
+
+//pub fn get_user_purchases(user_id) -> vec<songs> {}
